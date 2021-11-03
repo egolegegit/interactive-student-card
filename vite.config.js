335d466cf4a10d6dda586b2cc637809ec695f4ec
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-11-03 12:06:52 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactRefresh()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
   },
